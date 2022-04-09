60467571d23f5f5fa896e71d426591a0ab753641
Joji <freedive.cebu30@gmail.com>
2022-04-09 12:36:23 +0800

    Create question_1.js

---
+++ question_1.js
@@ -0,0 +1,12 @@
+let number_1 = 10;
+let number_2 = 3;
+let number_3 = 2;
+let number_4 = 10000;
+  
+console.log(`${number_1} + ${number_2} = ${number_1 + number_2}`);
+console.log(`${number_1} - ${number_2} = ${number_1 - number_2}`);
+console.log(`${number_1} * ${number_2} = ${number_1 * number_2}`);
+console.log(`${number_4} / ${number_1} = ${number_4 / number_1}`);
+console.log(`${number_1} / ${number_2} = ${number_1 / number_2} 余り ${number_1 % number_2}`);
+console.log(`${number_3}の2乗 = ${number_3 ** 2}`);
+console.log(`${number_3}の3乗 = ${number_3 ** 3}`);
