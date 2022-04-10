38fc3be851352115ba37d58ffcce09f529b2c031
Joji <freedive.cebu30@gmail.com>
2022-04-10 16:06:28 +0800

    Update question_21.js

--- question_21.js
+++ question_21.js
@@ -1,8 +1,6 @@
 let numbers = [1, 12, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3];
 let set1 = new Set(numbers);
 let uniqNumbers = Array.from(set1);
-uniqNumbers.sort(function(v1, v2) {
-  return v2 - v1;
-});
+uniqNumbers.sort((v1, v2) => v2 - v1);
 
 console.log(uniqNumbers);
