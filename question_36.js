6320c635b1939bad4bda966f3489b105c5040967
Joji <freedive.cebu30@gmail.com>
2022-04-12 11:45:36 +0800

    Update question_36.js

--- question_36.js
+++ question_36.js
@@ -1,4 +1,4 @@
-let name = new Map([["name", "Taro"], ["age", 20], ["height", 180], ["weight", 60]]);
-name.set('nick_name', 'Ace');
+let human = new Map([["name", "Taro"], ["age", 20], ["height", 180], ["weight", 60]]);
+human.set('nick_name', 'Ace');
 
-console.log(name);
+console.log(human);
