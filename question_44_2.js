1cb6be87eba615070bfc3687857e39971f4d563e
Joji <freedive.cebu30@gmail.com>
2022-04-12 21:19:39 +0800

    Create question_44_2.js

---
+++ question_44_2.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "apple", "orange", "mango", "mango", "mango", "mango"];
+let mapFruits = {};
+let current_num = 0;
+for (let fruit of fruits) {
+  if (isNaN(mapFruits[fruit])) {
+    mapFruits[fruit] = 1;
+  } else {
+    mapFruits[fruit] += 1;   
+  }
+}
+console.log(mapFruits);
