d3128b49f19a9fe7f361e8def3ecf8553d36222b
Joji <freedive.cebu30@gmail.com>
2022-04-12 21:11:30 +0800

    Create question_44.js

---
+++ question_44.js
@@ -0,0 +1,14 @@
+let fruits = ["apple", "apple", "orange", "mango", "mango", "mango", "mango"];
+let mapFruits = new Map();
+let current_num = 0;
+for (let fruit of fruits) {
+  if (mapFruits.has(fruit)) {
+    current_num = mapFruits.get(fruit);
+  } else {
+    current_num = 0; 
+  }
+  
+  console.log(current_num);
+  mapFruits.set(fruit, current_num + 1);
+}
+console.log(mapFruits);
