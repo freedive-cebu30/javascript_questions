78d86270ef12f21f731e8a31c3f868aab483ab94
Joji <freedive.cebu30@gmail.com>
2022-06-04 12:47:25 +0800

    Update question_22.js

--- question_22.js
+++ question_22.js
@@ -1,4 +1,4 @@
-languages = ["ruby", "php", "python", "javaScript"];
+let languages = ["ruby", "php", "python", "javaScript"];
 languages.sort((n1, n2) => n1.length - n2.length);
 
 console.log(languages);
